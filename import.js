2429c5cd5e032e6d8a60fac3025816f2e83bf92a
Mirko Di Serafino <mirko.diserafino@stamplay.com>
2014-06-02 15:42:40 +0200

    added error as comment

--- import.js
+++ import.js
@@ -9,7 +9,9 @@ var parser = binaryCSV({separator:'\t', json: true});
 var through2 = require('through2')
 var nodes = [];
 
-fs.createReadStream('./amazon0601.txt').pipe(parser);
+
+// FATAL ERROR: CALL_AND_RETRY_2 Allocation failed - process out of memory
+fs.createReadStream('./amazon0601.txt',{highWaterMark:16}).pipe(parser);
 parser.pipe(through2({objectMode: true}, function (chunk, enc, callback) {
   
   var data = {
@@ -19,7 +21,7 @@ parser.pipe(through2({objectMode: true}, function (chunk, enc, callback) {
   }
   this.push(data)
   callback();
-}),{highWaterMark:16}).pipe(db.putStream(),{highWaterMark:16});
+})).pipe(db.putStream());
 
 // db.get({},function(err, list){ 
 //   console.log(list.length);
