b1f2203b04b3743984ae3eb43d5eeff60c651f66
Matteo Collina <matteo.collina@gmail.com>
2014-06-02 16:13:09 +0200

    GC-friendly import.

--- import.js
+++ import.js
@@ -1,27 +1,36 @@
 var fs         = require('fs')
   , levelgraph = require("levelgraph")
   , async     = require("async")
-  , db         = levelgraph("importedLG");
+  , binaryCSV = require('binary-csv')
+  , through2 = require('through2')
+
+levelgraph("importedLG", function(err, db) {
+
+  var parser = binaryCSV({separator:'\t', json: true})
+    , count  = 0;
+
+  // FATAL ERROR: CALL_AND_RETRY_2 Allocation failed - process out of memory
+  fs.createReadStream('./amazon0601.txt').pipe(parser);
+  parser.pipe(through2({objectMode: true, highWaterMark:16}, function (chunk, enc, callback) {
+    
+    var data = {
+      subject: chunk.from,
+      predicate : 'similar',
+      object: chunk.to
+    }
+    this.push(data)
+
+    if (++count % 1000 === 0) {
+      console.log(count)
+      // let the GC run from time to time
+      setImmediate(callback);
+    } else {
+      callback()
+    }
+  })).pipe(db.putStream({highWaterMark:16}));
+});
 
 
-var binaryCSV = require('binary-csv')
-var parser = binaryCSV({separator:'\t', json: true});
-var through2 = require('through2')
-var nodes = [];
-
-
-// FATAL ERROR: CALL_AND_RETRY_2 Allocation failed - process out of memory
-fs.createReadStream('./amazon0601.txt',{highWaterMark:16}).pipe(parser);
-parser.pipe(through2({objectMode: true}, function (chunk, enc, callback) {
-  
-  var data = {
-    subject: chunk.from,
-    predicate : 'similar',
-    object: chunk.to
-  }
-  this.push(data)
-  callback();
-})).pipe(db.putStream());
 
 // db.get({},function(err, list){ 
 //   console.log(list.length);
