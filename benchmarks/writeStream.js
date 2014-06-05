7bdb1ac43ae582326d56ad2ccea2f094984f1d92
Claudio Petrini <claudio.petrini@stamplay.com>
2014-06-05 15:53:14 +0200

    Added 80k rows dataset

--- benchmarks/writeStream.js
+++ benchmarks/writeStream.js
@@ -1,40 +1,49 @@
-var fs         = require('fs')
-  , levelgraph = require("levelgraph")
-  , async     = require("async")
-  , binaryCSV = require('binary-csv')
-  , through2 = require('through2')
-  , db = levelgraph("importedLG");
+var fs = require('fs'),
+  levelgraph = require("levelgraph"),
+  async = require("async"),
+  binaryCSV = require('binary-csv'),
+  through2 = require('through2');
 
-levelgraph("../importedLG", function(err, db) {
+levelgraph("../importedLG", function (err, db) {
 
-  var parser = binaryCSV({separator:'\t', json: true})
-    , startTime = new Date()
-    , count  = 0;
-  
-  
-  fs.createReadStream('../amazon0601.txt').pipe(parser);
-  parser.pipe(through2({objectMode: true, highWaterMark:16}, function (chunk, enc, callback) {
-    
+  var parser = binaryCSV({
+      separator: '\t',
+      json: true
+    }),
+    startTime = new Date(),
+    count = 0;
+
+
+  // fs.createReadStream('../amazon0601.txt').pipe(parser);
+  fs.createReadStream('../amazonSmall.txt').pipe(parser);
+
+  parser.pipe(through2({
+    objectMode: true,
+    highWaterMark: 16
+  }, function (chunk, enc, callback) {
+
     var data = {
       subject: chunk.from,
-      predicate : 'similar',
+      predicate: 'similar',
       object: chunk.to
     }
     this.push(data)
 
     if (++count % 10000 === 0) {
-      
+
       setImmediate(callback);
     } else {
       callback()
     }
   }))
-  .pipe(db.putStream({highWaterMark:16})
-    .on("close", function() {
-      endTime = new Date();
-      var totalTime = endTime - startTime;
-      console.log("total time", totalTime);
-      console.log("writes/s", count / totalTime * 1000);
-    })
+    .pipe(db.putStream({
+        highWaterMark: 16
+      })
+      .on("close", function () {
+        endTime = new Date();
+        var totalTime = endTime - startTime;
+        console.log("total time", totalTime);
+        console.log("writes/s", count / totalTime * 1000);
+      })
   );
 });
